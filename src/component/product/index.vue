51544cc673b9f9a18ce40f7275a57a7f5e9947dd
zhoujianlong <zjl_606@aliyun.com>
2019-12-25 17:36:26 +0800

    修改上一个提交的bug

--- src/component/product/index.vue
+++ src/component/product/index.vue
@@ -24,7 +24,7 @@
                         <sp-product-lsb :product="row"></sp-product-lsb>
                     </template>
                     <template v-slot:productname="{row}">
-                        <el-link type="primary" @click.stop="showFormToEdit(row)">{{row.getName()}}</el-link>
+                        <el-link type="primary" @click.stop="showFormToEdit(0, row)">{{row.getName()}}</el-link>
                     </template>
                     <template v-slot:series="{row}">
                         <sp-select-text :value="row.series" source="series" />
@@ -33,7 +33,7 @@
             </el-col>
         </el-row>
         <asa-product ref="product" @change="search"></asa-product>
-        <asa-product-add ref="productadd" @change="search" @editExistProduct="showFormToEdit(arguments)"></asa-product-add>
+        <asa-product-add ref="productadd" @change="search" @editExistProduct="showFormToEdit(0, arguments)"></asa-product-add>
         <sp-dialog ref="search">
             <sp-product-search-form @search="onSearch" @close="_hideDialog('search')"></sp-product-search-form>
         </sp-dialog>
@@ -163,8 +163,8 @@ export default {
             self.$refs.tablelist.search(self.form);
             self._hideDialog("search");
         },
-        showFormToEdit(row) {
-            this.$refs.product.setInfo(row).then(product => product.edit(true).show());
+        showFormToEdit(rowIndex, row) {
+            this.$refs.product.setInfo(row).then(product => product.edit(true).show())
         },
         showFormToCreate() {
             this.$refs.productadd.show();
