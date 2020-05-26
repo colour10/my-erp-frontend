e6bdb141f2b4fbefe93f926a49d4465ccec1a6fa
colour10 <84299915@qq.com>
2020-05-26 15:00:22 +0800

    操作框尺码修改

--- src/component/user/setting.vue
+++ src/component/user/setting.vue
@@ -1,50 +1,56 @@
 <template>
-    <el-tabs type="border-card" activeName="setting">
-        <el-tab-pane :label="_label('morenshezhi')" name="setting">
-            <el-form class="order-form" :model="form" label-width="85px" :inline="true" style="width:700px;" size="small">
-                <el-form-item :label="_label('xiaoshouduankou')">
-                    <simple-select v-model="form.saleportid" source="usersaleport"></simple-select>
-                </el-form-item>
-                <el-form-item :label="_label('jiage')">
-                    <simple-select v-model="form.priceid" source="userprice"></simple-select>
-                </el-form-item>
-                <el-form-item :label="_label('xiaoshoucangku')">
-                    <simple-select v-model="form.warehouseid" source="userwarehouse"></simple-select>
-                </el-form-item>
-            </el-form>
-            <as-button type="primary" @click="onSubmit">{{_label('baocun')}}</as-button>
-        </el-tab-pane>
-    </el-tabs>
+  <el-tabs type="border-card" activeName="setting">
+    <el-tab-pane :label="_label('morenshezhi')" name="setting">
+      <el-form class="order-form" :model="form" label-width="85px" :inline="true" style="width:700px;" size="small">
+        <el-form-item :label="_label('xiaoshouduankou')">
+          <simple-select v-model="form.saleportid" source="usersaleport"></simple-select>
+        </el-form-item>
+        <el-form-item :label="_label('jiage')">
+          <simple-select v-model="form.priceid" source="userprice"></simple-select>
+        </el-form-item>
+        <el-form-item :label="_label('xiaoshoucangku')">
+          <simple-select v-model="form.warehouseid" source="userwarehouse"></simple-select>
+        </el-form-item>
+      </el-form>
+      <as-button type="primary" @click="onSubmit">{{_label('baocun')}}</as-button>
+    </el-tab-pane>
+  </el-tabs>
 </template>
 
 <script>
-import API from '../api.js';
+    import API from '../api.js';
 
-export default {
-    name: 'sp-usersetting',
-    data() {
-        return {
-            form: {
-                saleportid: '',
-                priceid: '',
-                warehouseid: '',
-            },
-        };
-    },
-    methods: {
-        onSubmit() {
-            let self = this;
-            self._submit("/user/setting", self.form).then(() => {});
+    export default {
+        name: 'sp-usersetting',
+        data() {
+            return {
+                form: {
+                    saleportid: '',
+                    priceid: '',
+                    warehouseid: '',
+                },
+            };
         },
-    },
-    async mounted() {
-        const self = this;
-        self._setTitle(self._label('gerenshezhi'));
+        methods: {
+            onSubmit() {
+                let self = this;
+                self._submit("/user/setting", self.form).then(() => {
+                });
+            },
+        },
+        async mounted() {
+            const self = this;
+            self._setTitle(self._label('gerenshezhi'));
 
-        let setting = await API.getSetting();
-        self.form.saleportid = setting.saleportid;
-        self.form.warehouseid = setting.warehouseid;
-        self.form.priceid = setting.priceid;
-    },
-};
+            let setting = await API.getSetting();
+            self.form.saleportid = setting.saleportid;
+            self.form.warehouseid = setting.warehouseid;
+            self.form.priceid = setting.priceid;
+
+
+            console.log("this.userprice=start")
+            console.log(this.userprice)
+            console.log("this.userprice=end")
+        },
+    };
 </script>
